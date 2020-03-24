de21e1f57de794f0744216233e6653d3a4218a56
haojianwei <1757009705@qq.com>
2020-03-24 18:32:09 +0800

    小尴尬

--- src/layout/sidebar/compeonts/SubMenu.vue
+++ src/layout/sidebar/compeonts/SubMenu.vue
@@ -2,7 +2,7 @@
   <div>
     <template v-for="(menu, index) in menus">
       <el-submenu v-if="menu.children && menu.children.length" :index="menu.index" :key="index">
-        <submenu :menus="menu.children"></submenu>
+        <sub-menu :menus="menu.children"></sub-menu>
       </el-submenu>
       <el-menu-item :index="menu.index" :key="index">{{menu.name}}</el-menu-item>
     </template>
